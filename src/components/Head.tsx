82955f763f3709c69d1a5f4d0e14ba0faa757db9
ruben <a.ruben00001@gmail.com>
2022-04-12 16:48:52 +0100

    test

---
+++ src/components/Head.tsx
@@ -0,0 +1,13 @@
+import NextHead from "next/head";
+
+const Head = () => {
+  return (
+    <NextHead>
+      <title>Asatic CMS</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="robots" content="noindex,nofollow" />
+    </NextHead>
+  );
+};
+
+export default Head;
