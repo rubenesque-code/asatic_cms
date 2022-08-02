1410b9cb6ab62e5b9ec2d0e12ae5e10ad4f6eeed
ruben <a.ruben00001@gmail.com>
2022-08-02 10:30:42 +0100

    .

--- src/components/WithCollections.tsx
+++ src/components/WithCollections.tsx
@@ -12,7 +12,7 @@ import tw from "twin.macro";
 import {
   ArrowElbowDownRight as ArrowElbowDownRightIcon,
   Books as BooksIcon,
-  FileMinus,
+  FileMinus as FileMinusIcon,
   FilePlus,
   Plus,
   Translate,
@@ -52,7 +52,8 @@ import {
 } from "^context/CollectionContext";
 import { fuzzySearchCollections } from "^helpers/collections";
 import { SubjectProvider } from "^context/SubjectContext";
-import WithDocSubjects from "./WithSubjects";
+import WithDocSubjectsInitial from "./WithSubjects";
+import { ContentMenuButton, ContentMenuVerticalBar } from "./menus/Content";
 
 type TopProps = {
   docActiveLanguageId: string;
@@ -117,17 +118,17 @@ const PanelUI = ({ areDocCollections }: { areDocCollections: boolean }) => (
     <div>
       <h4 css={[tw`font-medium text-lg`]}>Collections</h4>
       <p css={[tw`text-gray-600 mt-xs text-sm`]}>
-        Collections allow groups of content to be grouped under a topic (rather
-        than a subject, which is broader). You can optionally relate a
-        collection to a subject(s).
+        Collections allows content to be grouped under a topic (rather than a
+        subject, which is broader). A collection can optionally be part of a
+        subject(s).
       </p>
       {!areDocCollections ? (
         <p css={[tw`text-gray-800 mt-xs text-sm`]}>None yet.</p>
       ) : null}
     </div>
-    <div css={[tw`flex flex-col gap-lg items-start`]}>
+    <div css={[tw`flex flex-col gap-md items-start`]}>
       <List />
-      {/* <InputWithSelect /> */}
+      <InputWithSelect />
     </div>
   </div>
 );
@@ -163,7 +164,9 @@ const ListItem = ({
 
   return (
     <ListItemUI
-      collection={<HandleCollection docCollectionId={docCollectionId} />}
+      collection={
+        <HandleCollectionValidity docCollectionId={docCollectionId} />
+      }
       number={number}
     />
   );
@@ -184,7 +187,11 @@ const ListItemUI = ({
   );
 };
 
-const HandleCollection = ({ docCollectionId }: { docCollectionId: string }) => {
+const HandleCollectionValidity = ({
+  docCollectionId,
+}: {
+  docCollectionId: string;
+}) => {
   const collection = useSelector((state) =>
     selectCollectionById(state, docCollectionId)
   );
@@ -194,16 +201,6 @@ const HandleCollection = ({ docCollectionId }: { docCollectionId: string }) => {
       <ValidCollection />
     </CollectionProvider>
   ) : (
-    <InvalidCollection docCollectionId={docCollectionId} />
-  );
-};
-
-const InvalidCollection = ({
-  docCollectionId,
-}: {
-  docCollectionId: string;
-}) => {
-  return (
     <InvalidCollectionUI
       removeFromDocButton={
         <RemoveFromDocButton docCollectionId={docCollectionId} />
@@ -258,36 +255,478 @@ const RemoveFromDocButtonUI = ({
   removeFromDoc: () => void;
   tooltipText: string;
   warningText: string;
-}) => {
-  return (
-    <WithWarning
-      callbackToConfirm={removeFromDoc}
-      warningText={{ heading: warningText }}
-      type="moderate"
+}) => (
+  <WithWarning
+    callbackToConfirm={removeFromDoc}
+    warningText={{ heading: warningText }}
+    type="moderate"
+  >
+    {({ isOpen: warningIsOpen }) => (
+      <ContentMenuButton
+        tooltipProps={{
+          isDisabled: warningIsOpen,
+          placement: "top",
+          text: tooltipText,
+          type: "action",
+        }}
+      >
+        <FileMinusIcon />
+      </ContentMenuButton>
+    )}
+  </WithWarning>
+);
+
+const ValidCollection = () => {
+  return <ValidCollectionUI />;
+};
+
+const ValidCollectionUI = () => (
+  <div css={[tw`flex items-center gap-sm`]} className="group">
+    <div
+      css={[
+        tw`opacity-0 group-hover:opacity-100 transition-opacity duration-150 ease-in delay-300`,
+      ]}
     >
-      {({ isOpen: warningIsOpen }) => (
-        <WithTooltip
-          text={tooltipText}
-          placement="top"
-          isDisabled={warningIsOpen}
-          type="action"
-        >
-          <button
-            css={[
-              tw`text-gray-600 p-xxs hover:bg-gray-100 hover:text-red-warning active:bg-gray-200 rounded-full grid place-items-center`,
-            ]}
-            type="button"
-          >
-            <FileMinus />
-          </button>
-        </WithTooltip>
-      )}
-    </WithWarning>
+      <ValidCollectionMenu />
+    </div>
+    <div
+      css={[
+        tw`translate-x-[-73px] group-hover:z-40 group-hover:translate-x-0 transition-transform duration-150 ease-in delay-300`,
+      ]}
+    >
+      <CollectionTranslations />
+    </div>
+  </div>
+);
+
+const ValidCollectionMenu = () => {
+  const [{ id }] = useCollectionContext();
+
+  return (
+    <ValidCollectionMenuUI
+      removeFromDocButton={<RemoveFromDocButton docCollectionId={id} />}
+    />
   );
 };
 
-const ValidCollection = () => {
-  return <CollectionUI />;
+const ValidCollectionMenuUI = ({
+  removeFromDocButton,
+}: {
+  removeFromDocButton: ReactElement;
+}) => (
+  <div css={[tw`flex items-center gap-xs`]}>
+    {removeFromDocButton}
+    <AddSubjectButton />
+    <ContentMenuVerticalBar />
+  </div>
+);
+
+const AddSubjectButton = () => {
+  return (
+    <WithDocSubjects>
+      <AddSubjectButtonUI />
+    </WithDocSubjects>
+  );
 };
 
-const CollectionUI = () => <div>Collection</div>;
+const AddSubjectButtonUI = () => (
+  <ContentMenuButton
+    tooltipProps={{ text: "add subject", placement: "top", type: "action" }}
+  >
+    <BooksIcon />
+  </ContentMenuButton>
+);
+
+const WithDocSubjects = ({ children }: { children: ReactElement }) => {
+  const { docActiveLanguageId, docLanguagesById } = useWithCollectionsContext();
+  const [{ subjectsById }, { removeSubject, addSubject }] =
+    useCollectionContext();
+
+  return (
+    <WithDocSubjectsInitial
+      docActiveLanguageId={docActiveLanguageId}
+      docLanguagesById={docLanguagesById}
+      docSubjectsById={subjectsById}
+      docType="collection"
+      onAddSubjectToDoc={(subjectId) => addSubject({ subjectId })}
+      onRemoveSubjectFromDoc={(subjectId) => removeSubject({ subjectId })}
+    >
+      {children}
+    </WithDocSubjectsInitial>
+  );
+};
+
+const CollectionTranslations = () => {
+  return <CollectionTranslationsUI />;
+};
+
+const CollectionTranslationsUI = () => <div>Translations</div>;
+
+const InputWithSelect = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const [inputIsFocused, focusHandlers] = useFocused();
+
+  return (
+    <InputWithSelectUI
+      input={
+        <Input
+          focusHandlers={focusHandlers}
+          setValue={setInputValue}
+          value={inputValue}
+        />
+      }
+      language={<InputLanguage show={inputIsFocused} />}
+      select={
+        <CollectionsSelect
+          query={inputValue}
+          show={inputValue.length > 1 && inputIsFocused}
+        />
+      }
+    />
+  );
+};
+
+const InputWithSelectUI = ({
+  input,
+  language,
+  select,
+}: {
+  input: ReactElement;
+  language: ReactElement;
+  select: ReactElement;
+}) => {
+  return (
+    <div css={[tw`relative w-full`]}>
+      <div css={[tw`relative inline-block`]}>
+        {input}
+        {language}
+      </div>
+      {select}
+    </div>
+  );
+};
+
+const Input = ({
+  focusHandlers,
+  setValue,
+  value,
+}: {
+  focusHandlers: {
+    onFocus: () => void;
+    onBlur: () => void;
+  };
+  setValue: Dispatch<SetStateAction<string>>;
+  value: string;
+}) => {
+  const { docActiveLanguageId, onAddCollectionToDoc } =
+    useWithCollectionsContext();
+
+  const dispatch = useDispatch();
+
+  const submitNewCollection = () => {
+    const id = generateUId();
+    dispatch(addOne({ id, text: value, languageId: docActiveLanguageId }));
+    onAddCollectionToDoc(id);
+    setValue("");
+  };
+
+  return (
+    <InputUI
+      focusHandlers={focusHandlers}
+      inputValue={value}
+      onChange={(e) => setValue(e.target.value)}
+      onSubmit={(e) => {
+        e.preventDefault();
+        submitNewCollection();
+      }}
+    />
+  );
+};
+
+const inputId = "collection-input";
+
+const InputUI = ({
+  focusHandlers,
+  inputValue,
+  onChange,
+  onSubmit,
+}: {
+  focusHandlers: {
+    onFocus: () => void;
+    onBlur: () => void;
+  };
+  inputValue: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+}) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <div css={[tw`relative`]}>
+        <input
+          css={[
+            tw`px-lg py-1 text-sm outline-none border-2 border-transparent focus:border-gray-200 rounded-sm`,
+          ]}
+          id={inputId}
+          value={inputValue}
+          onChange={onChange}
+          placeholder="Add a new collection..."
+          type="text"
+          autoComplete="off"
+          {...focusHandlers}
+        />
+        <label
+          css={[tw`absolute left-2 top-1/2 -translate-y-1/2 text-gray-500`]}
+          htmlFor={inputId}
+        >
+          <Plus />
+        </label>
+      </div>
+    </form>
+  );
+};
+
+const InputLanguage = ({ show }: { show: boolean }) => {
+  const { docActiveLanguageId } = useWithCollectionsContext();
+
+  const language = useSelector((state) =>
+    selectLanguageById(state, docActiveLanguageId)
+  );
+
+  return (
+    <InputLanguageUI
+      languageText={language ? language.name : <LanguageError />}
+      show={show}
+    />
+  );
+};
+
+const InputLanguageUI = ({
+  languageText,
+  show,
+}: {
+  languageText: ReactElement | string;
+  show: boolean;
+}) => {
+  return (
+    <div
+      css={[
+        tw`absolute top-2 right-0 -translate-y-full flex items-center gap-xxs bg-white`,
+        s_transition.toggleVisiblity(show),
+        tw`transition-opacity duration-75 ease-in-out`,
+      ]}
+    >
+      <span css={[tw`text-sm -translate-y-1 text-gray-400`]}>
+        <Translate weight="light" />
+      </span>
+      <span css={[tw`capitalize text-gray-400 text-sm`]}>{languageText}</span>
+    </div>
+  );
+};
+
+const CollectionsSelect = ({
+  query,
+  show,
+}: {
+  query: string;
+  show: boolean;
+}) => {
+  const allCollections = useSelector(selectAll);
+
+  const collectionsMatchingQuery = fuzzySearchCollections(
+    query,
+    allCollections
+  );
+
+  return (
+    <CollectionsSelectUI
+      collectionsMatchingQuery={
+        <CollectionsMatchingQuery
+          collectionMatches={collectionsMatchingQuery}
+        />
+      }
+      show={show}
+    />
+  );
+};
+
+const CollectionsSelectUI = ({
+  collectionsMatchingQuery,
+  show,
+}: {
+  collectionsMatchingQuery: ReactElement;
+  show: boolean;
+}) => {
+  return (
+    <div
+      css={[
+        tw`absolute -bottom-2 translate-y-full w-full bg-white border-2 border-gray-200 rounded-sm py-sm text-sm shadow-lg`,
+        show ? tw`opacity-100` : tw`opacity-0 h-0`,
+        tw`transition-opacity duration-75 ease-linear`,
+      ]}
+    >
+      {collectionsMatchingQuery}
+    </div>
+  );
+};
+
+const CollectionsMatchingQuery = ({
+  collectionMatches: collectionMatches,
+}: {
+  collectionMatches: CollectionType[];
+}) => {
+  return (
+    <CollectionsMatchingQueryUI
+      areMatches={Boolean(collectionMatches.length)}
+      collectionMatches={
+        <>
+          {collectionMatches.map((a) => (
+            <CollectionMatch collection={a} key={a.id} />
+          ))}
+        </>
+      }
+    />
+  );
+};
+
+const CollectionsMatchingQueryUI = ({
+  areMatches,
+  collectionMatches,
+}: {
+  areMatches: boolean;
+  collectionMatches: ReactElement;
+}) => {
+  return (
+    <div css={[tw`flex flex-col gap-xs items-start`]}>
+      {areMatches ? (
+        collectionMatches
+      ) : (
+        <p css={[tw`text-gray-600 ml-sm`]}>No matches</p>
+      )}
+    </div>
+  );
+};
+
+const CollectionMatch = ({ collection }: { collection: CollectionType }) => {
+  const { docCollectionsById, onAddCollectionToDoc } =
+    useWithCollectionsContext();
+  const { id, translations } = collection;
+  const isDocCollection = docCollectionsById.includes(id);
+
+  return (
+    <CollectionMatchUI
+      addCollectionToDoc={() => !isDocCollection && onAddCollectionToDoc(id)}
+      canAddToDoc={!isDocCollection}
+      translations={<CollectionMatchTranslations translations={translations} />}
+    />
+  );
+};
+
+const CollectionMatchUI = ({
+  addCollectionToDoc,
+  canAddToDoc,
+  translations,
+}: {
+  addCollectionToDoc: () => void;
+  canAddToDoc: boolean;
+  translations: ReactElement;
+}) => {
+  return (
+    <WithTooltip
+      text="add collection to document"
+      type="action"
+      isDisabled={!canAddToDoc}
+    >
+      <button
+        css={[
+          tw`text-left py-1 relative w-full px-sm`,
+          !canAddToDoc && tw`pointer-events-none`,
+        ]}
+        className="group"
+        onClick={addCollectionToDoc}
+        type="button"
+      >
+        <span
+          css={[
+            tw`text-gray-600 group-hover:text-gray-800`,
+            !canAddToDoc && tw`text-gray-400`,
+          ]}
+        >
+          {translations}
+        </span>
+        {canAddToDoc ? (
+          <span
+            css={[
+              s_transition.onGroupHover,
+              tw`group-hover:z-50 bg-white absolute right-2 top-1/2 -translate-y-1/2 text-green-600`,
+            ]}
+          >
+            <FilePlus weight="bold" />
+          </span>
+        ) : null}
+      </button>
+    </WithTooltip>
+  );
+};
+
+// text overflow - have ellipsis ideally
+const CollectionMatchTranslations = ({
+  translations,
+}: {
+  translations: CollectionTranslationType[];
+}) => {
+  const validTranslations = translations.filter((t) => t.text.length);
+  return (
+    <CollectionMatchTranslationsUI
+      translations={
+        <>
+          {validTranslations.map((t, i) => (
+            <CollectionMatchTranslation index={i} translation={t} key={t.id} />
+          ))}
+        </>
+      }
+    />
+  );
+};
+
+const CollectionMatchTranslationsUI = ({
+  translations,
+}: {
+  translations: ReactElement;
+}) => {
+  return (
+    <div css={[tw`flex items-center gap-xs overflow-hidden`]}>
+      {translations}
+    </div>
+  );
+};
+
+const CollectionMatchTranslation = ({
+  index,
+  translation,
+}: {
+  index: number;
+  translation: CollectionType["translations"][number];
+}) => {
+  return (
+    <CollectionMatchTranslationUI
+      isFirst={index === 0}
+      text={translation.text}
+    />
+  );
+};
+
+const CollectionMatchTranslationUI = ({
+  isFirst,
+  text,
+}: {
+  isFirst: boolean;
+  text: string;
+}) => {
+  return (
+    <div css={[tw`flex items-center gap-xs`]}>
+      {!isFirst ? <span css={[tw`w-[0.5px] h-[15px] bg-gray-200`]} /> : null}
+      <p>{text}</p>
+    </div>
+  );
+};
