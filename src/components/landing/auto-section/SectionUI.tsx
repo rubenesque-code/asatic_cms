0dbbb2334663fd67bad0712998b760853c303a69
ruben <a.ruben00001@gmail.com>
2022-08-15 18:06:03 +0100

    .

--- src/components/landing/auto-section/SectionUI.tsx
+++ src/components/landing/auto-section/SectionUI.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from "phosphor-react";
 import { ReactElement } from "react";
 import tw from "twin.macro";
 
@@ -6,22 +7,28 @@ import { LandingColorTheme } from "^types/landing";
 
 const AutoSectionUI = ({
   colorTheme,
+  moreFromText,
   swiper,
   title,
 }: {
   colorTheme: LandingColorTheme;
   title: string;
+  moreFromText: string;
   swiper: ReactElement;
 }) => (
   <div css={[tw`font-serif-eng`, landingColorThemes[colorTheme].bg]}>
-    <h3
+    <div
       css={[
-        tw`pl-xl pt-sm pb-xs text-2xl border-b`,
         landingColorThemes[colorTheme].text,
+        tw`flex items-center justify-between`,
       ]}
     >
-      {title}
-    </h3>
+      <h3 css={[tw`pl-xl pt-sm pb-xs text-2xl border-b`]}>{title}</h3>
+      <p css={[tw`flex items-center gap-xs text-lg mr-lg`]}>
+        <span>{moreFromText}</span>
+        <ArrowRight weight="bold" />
+      </p>
+    </div>
     <div css={[tw`ml-lg z-10 border-l`]}>{swiper} </div>
   </div>
 );
