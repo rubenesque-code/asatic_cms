7af1dff3130516565673a3d9bb3c0b9f734e1902
ruben <a.ruben00001@gmail.com>
2022-11-21 13:23:26 +0000

    .

--- src/components/pages/curated/subject/document/body/populated/entity/_styles/index.ts
+++ src/components/pages/curated/subject/document/body/populated/entity/_styles/index.ts
@@ -6,14 +6,14 @@ export const $status = tw`mb-sm`;
 
 export const $SummaryContainer = tw.div``;
 
-export const $Title = tw.div`text-gray-800 text-xl font-serif-eng cursor-pointer`;
+export const $Title = tw.div`text-gray-800 text-xl font-serif-eng cursor-default`;
 
-export const $Authors = tw.div`text-gray-500 text-lg font-serif-eng mt-xxs mb-sm cursor-pointer`;
+export const $Authors = tw.div`text-gray-500 text-lg font-serif-eng mt-xxs mb-sm cursor-default`;
 
-export const $Date = tw.div`text-gray-700 font-sans uppercase text-sm tracking-wider mb-sm cursor-pointer`;
+export const $Date = tw.div`text-gray-700 font-sans uppercase text-sm tracking-wider mb-sm cursor-default`;
 
-export const $Text = tw.div`text-gray-800 text-base font-serif-eng`;
+export const $Text = tw.div`text-gray-800 text-base font-serif-eng cursor-text`;
 
-export const $RecordedEventType = tw.div`uppercase mb-xs text-sm tracking-widest cursor-pointer`;
+export const $RecordedEventType = tw.div`uppercase mb-xs text-sm tracking-widest cursor-default`;
 
 export const $recordedEventImage = tw`relative aspect-ratio[ 9 / 5] mb-sm`;
