4905e60fcf9b773aba59bdff2cab947ab19a3358
ruben00001 <a.ruben00001@gmail.com>
2023-02-02 18:20:18 +0000

    .

--- src/components/pages/curated/document/_styles/$articleLikeDocument.ts
+++ src/components/pages/curated/document/_styles/$articleLikeDocument.ts
@@ -10,6 +10,6 @@ export const $Title = tw.div`text-3xl font-medium w-full font-serif-eng`;
 
 export const $Authors = tw.div`text-xl font-serif-eng cursor-pointer`;
 
-export const $BodyPopulatedContainer = tw.div`mt-md`;
+export const $BodyPopulatedContainer = tw.div`mt-md flex flex-col gap-md`;
 
 export const $Caption = tw.div`mt-xs border-l-2 border-gray-400 pl-xs text-gray-600`;
